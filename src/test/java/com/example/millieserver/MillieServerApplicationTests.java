b872e0dca06229eb861b676a1cdf55809a38c2f2
jang-yujin <jyj9747@naver.com>
2023-07-18 17:21:57 +0900

    :white_check_mark: ContextLoads() 테스트 임시 주석 처리
    ec2 환경에서 빌드가 되지 않아 임시 주석 처리

--- src/test/java/com/example/millieserver/MillieServerApplicationTests.java
+++ src/test/java/com/example/millieserver/MillieServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MillieServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
